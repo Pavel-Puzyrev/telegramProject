6cd828e3e127924abfe465bda893edc0f5722eb4
Pavel Puzyrev <pavel.i.puzyrev@avianik.com>
2024-04-14 14:27:29 +0500

    feat: write through scheme

--- src/app/main.py
+++ src/app/main.py
@@ -1,11 +1,12 @@
 import encodings
+import json
 from typing import Optional, Any, Type, TypeVar, List
 import pprint
 from pydantic import BaseModel, Field
 
-file = "C:\\IT\\telegramProject\\src\\banya.json"
+# file = "C:\\IT\\telegramProject\\src\\banya.json"
 # file = "C:\\IT\\telegramProject\\src\\result1.json"
-# file = "C:\\IT\\telegramProject\\src\\derzi_v_kurse.json"
+file = "C:\\IT\\telegramProject\\src\\derzi_v_kurse.json"
 
 
 with open(file, encoding='utf-8') as f:
@@ -77,34 +78,34 @@ class Message(BaseModel):
 
 
 class MessageSet(BaseModel):
-    id: set[int]
-    type: set[str]
-    date: set[str]
-    date_unixtime: set[str]
-    edited: set[str | None]
-    edited_unixtime: set[str | None]
-    from_: set[str | None] = Field(alias='from')
-    from_id: set[str | None]
-    reply_to_message_id: set[int | None]
-    poll: set[Poll | None]
-    message_id: set[int | None]
-    forwarded_from: set[str | None]
-    photo: set[str | None]
-    file: set[str | None]
-    thumbnail: set[str | None]
-    media_type: set[str | None]
-    mime_type: set[str | None]
-    duration_seconds: set[int | None]
-    width: set[int | None]
-    height: set[int | None]
-    sticker_emoji: set[str | None]
-    text: set[str | list[str | TextItem]]
-    text_entities: set[list[TextEntity]]
-    actor: set[str | None]
-    actor_id: set[str | None]
-    action: set[str | None]
-    title: set[str | None]
-    members: set[list[Any]]
+    id: set[int] = set()
+    type: set[str] = set()
+    date: set[str] = set()
+    date_unixtime: set[str] = set()
+    edited: set[str | None] = set()
+    edited_unixtime: set[str | None] = set()
+    from_: set[str | None] = Field(default=set(), alias='from')
+    from_id: set[str | None] = set()
+    reply_to_message_id: set[int | None] = set()
+    poll: set[Poll | None] = set()
+    message_id: set[int | None] = set()
+    forwarded_from: set[str | None] = set()
+    photo: set[str | None] = set()
+    file: set[str | None] = set()
+    thumbnail: set[str | None] = set()
+    media_type: set[str | None] = set()
+    mime_type: set[str | None] = set()
+    duration_seconds: set[int | None] = set()
+    width: set[int | None] = set()
+    height: set[int | None] = set()
+    sticker_emoji: set[str | None] = set()
+    text: set[str | list[str | TextItem]] = set()
+    text_entities: set[list[TextEntity]] = set()
+    actor: set[str | None] = set()
+    actor_id: set[str | None] = set()
+    action: set[str | None] = set()
+    title: set[str | None] = set()
+    members: set[list[str | None]] = set()
 
 
 class DialogModel(BaseModel):
@@ -120,9 +121,11 @@ print(fake_db.name)
 print(fake_db.type)
 
 all_messages: list[Message] = fake_db.messages
+message_set = MessageSet()
 
 
 def print_field_gamut(attr_name: str, list_of_schemes: list[BaseModel]) -> str:
+    global message_set
     # if attr_name.find("text") != -1 or attr_name.find("date") != -1 or attr_name.find("id") != -1:
     #     return None
     _ = set()
@@ -131,28 +134,83 @@ def print_field_gamut(attr_name: str, list_of_schemes: list[BaseModel]) -> str:
         if attr_name is not None:
             attr = getattr(scheme_obj, attr_name)
         if attr is None:
-            _.add(None)
+            add_to_set = getattr(message_set, attr_name)
+            add_to_set.add(None)
+            setattr(message_set, attr_name, add_to_set)
+            # _.add(None)
         if not isinstance(attr, list | dict | set | tuple | BaseModel):
-            _.add(attr)
+            add_to_set = getattr(message_set, attr_name)
+            add_to_set.add(attr)
+            setattr(message_set, attr_name, add_to_set)
+            # _.add(attr)
         else:
-            _.add(type(attr))
+            add_to_set = getattr(message_set, attr_name)
+            add_to_set.add(type(attr))
+            setattr(message_set, attr_name, add_to_set)
+
+            # _.add(type(attr))
         # else: print("oh, fuck")
     # pprint.pprint(_)
     # print(f"{attr_name}:{_ if len(_) > 0 else " ALWAYS NONE"}")
-    return f"{attr_name}:{_}"
+    # return f"{attr_name}:{_}"
+    return getattr(message_set, attr_name)
 
 
-def write_to_file(list_of_schemes: list[BaseModel]):
-    file_out = "C:\\IT\\telegramProject\\src\\banya_out.json"
-    with open(file_out, encoding="utf_8", mode="w") as fw:
-        fw.write('{\n')
-        # for attr_name in scheme_class.__fields__.keys():
-        for attr_name in list_of_schemes[0].__fields__.keys():
-            fw.write(f"{print_field_gamut(attr_name, list_of_schemes)},\n")
-        fw.write('}')
+def write_to_file(list_of_schemes: list[BaseModel]) -> list[str]:
+    string_input = []
+    # for attr_name in scheme_class.__fields__.keys():
+    for attr_name in list_of_schemes[0].__fields__.keys():
+        string_input.append(print_field_gamut(attr_name, list_of_schemes))
+    return string_input
 
 
-write_to_file(all_messages)
+file_out = "C:\\IT\\telegramProject\\src\\banya_out.json"
+list_to_write = write_to_file(all_messages)
+with open(file_out, encoding="utf_8", mode="w") as fw:
+    # fw.write("\n".join(list_to_write))
+    # fw.write(message_set.model_dump(by_alias=True, exclude_none=True))
+    fw.write(message_set.model_dump_json(
+        by_alias=True,
+        exclude={
+            "poll",
+            "text",
+            "text_entities",
+            "members",
+            
+            "id",
+            "date",
+            "date_unixtime",
+            "edited",
+            "edited_unixtime",
+            "message_id",
+            "reply_to_message_id",
+            "forwarded_from",
+        },
+        indent=None
+    ))
+
+# from_
+# from_id
+# reply_to_message_id
+# poll
+# message_id
+# forwarded_from
+# photo
+# file
+# thumbnail
+# media_type
+# mime_type
+# duration_seconds
+# width
+# height
+# sticker_emoji
+# text
+# text_entities
+# actor
+# actor_id
+# action
+# title
+# members
 
 # fake_db_dict = fake_db.model_dump()
 # pprint.pprint(fake_db_dict)
